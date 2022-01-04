1a6b56b03ff792d0c49fa43c768f98ce50e91a77
CedBri <cedric.brisson@protonmail.com>
2022-01-03 19:23:45 -0500

    update

--- parser.go
+++ parser.go
@@ -1,6 +1,11 @@
 package link
 
-import "io"
+import (
+	"fmt"
+	"io"
+
+	"golang.org/x/net/html"
+)
 
 // Link is a link (<a href="...">) in an HTML page
 type Link struct {
@@ -11,5 +16,17 @@ type Link struct {
 // Parse takes an HTML page and returns
 // a slice of links or an error.
 func Parse(r io.Reader) ([]Link, error) {
+	doc, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+	dfs(doc, "")
 	return nil, nil
+}
+
+func dfs(n *html.Node, padding string) {
+	fmt.Println(padding, n.Data)
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		dfs(c, padding+"  ")
+	}
 }
